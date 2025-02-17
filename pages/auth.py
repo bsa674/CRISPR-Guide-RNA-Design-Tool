77ca437f6212ca9cace1ab515dfd087f9decd6d2
Pablo Alarcón <vastolorde97@gmail.com>
2025-02-18 00:51:14 +0100

    comments auth.py

--- pages/auth.py
+++ pages/auth.py
@@ -1,6 +1,16 @@
+'''
+Author : Pablo Alarcón
+
+File description :
+This file contains the get_auth_credentials() function, which is used in the login.py file to retrieve user credentials from the Firestore database.
+This function handles credential retrieval, while the Firestore database is loaded in login.py.
+
+Date of modified : 18th February, 2025
+'''
+#Load required libraries and modules
 import streamlit as st
 from pages.signup import fetch_users  # Ensure fetch_users() is correctly defined
-
+#Function
 def get_auth_credentials():
     users = fetch_users()
     if not users:
