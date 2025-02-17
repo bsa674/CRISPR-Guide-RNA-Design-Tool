aa0320ae7bbe1397b476db9e980f53c5ca7db26e
Bishal Samanta <79316570+bsa674@users.noreply.github.com>
2025-02-17 20:54:25 +0100

    Update admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -107,7 +107,7 @@ def regenerate_model():
                                 st.write(f"Mean Absolute Error: {mae}")
                                 st.write(f"R2 Score: {r2}")
                                 print('Plotting')
-                                for plot in os.listdir('feature_plots'):
+                                for plot in os.listdir('Backend/feature_plots'):
                                     st.image(f'Backend/feature_plots/{plot}')  # Display all plots
 
                 #                deploy = st.radio("Do you want to deploy this model for your users?", ('No', 'Yes'),index=None)
