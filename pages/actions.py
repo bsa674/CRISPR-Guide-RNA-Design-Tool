5e3670b29cce499c5a9b3db68396cbb74f1f2d94
raghvendra44 <68062751+raghvendra44@users.noreply.github.com>
2025-02-18 08:25:20 +0100

    Update actions.py
    Added Error handling

--- pages/actions.py
+++ pages/actions.py
@@ -25,73 +25,76 @@ def new_project(name = ''):
     data = None
     results = 0
     fasta_data = ''
-    with st.form(f"upload_func_{ss.page_state}"):
-        st.markdown("## Insert FASTA Sequence",help="Upload DNA sequence in FASTA format")
-        project_name = st.text_input("Project Name", value='Project 1' if name == '' else name,help='Set a Unique Name for future accessibility').strip()
-
-        if ss.page_state != 'modify' and name.strip() == '':
-            if not ss.data['Summary'].empty:
-                selectbox_style()
-                option = st.selectbox('Previously Loaded Project Inputs',(ss.data['Summary']['Project Name']),index=None)
-
-            st.markdown('<div style="text-align: center;">or</div>', unsafe_allow_html=True)
-            fasta_file = st.file_uploader("Upload FASTA File",type=['txt','fasta'],help='Click on upload and select fasta/text file of your choice')
-
-            st.markdown('<div style="text-align: center;">or</div>', unsafe_allow_html=True)
-
-            if option:
-                sequence = ss.data['Summary'][ss.data['Summary']["Project Name"]==option]['Sequence'].iloc[0]
-                data = f'>{option}\n{sequence}'
-                fasta_text = st.text_area("Insert DNA sequence as a FASTA format",value = data)
+    try:
+        with st.form(f"upload_func_{ss.page_state}"):
+            st.markdown("## Insert FASTA Sequence",help="Upload DNA sequence in FASTA format")
+            project_name = st.text_input("Project Name", value='Project 1' if name == '' else name,help='Set a Unique Name for future accessibility').strip()
+    
+            if ss.page_state != 'modify' and name.strip() == '':
+                if not ss.data['Summary'].empty:
+                    selectbox_style()
+                    option = st.selectbox('Previously Loaded Project Inputs',(ss.data['Summary']['Project Name']),index=None)
+    
+                st.markdown('<div style="text-align: center;">or</div>', unsafe_allow_html=True)
+                fasta_file = st.file_uploader("Upload FASTA File",type=['txt','fasta'],help='Click on upload and select fasta/text file of your choice')
+    
+                st.markdown('<div style="text-align: center;">or</div>', unsafe_allow_html=True)
+    
+                if option:
+                    sequence = ss.data['Summary'][ss.data['Summary']["Project Name"]==option]['Sequence'].iloc[0]
+                    data = f'>{option}\n{sequence}'
+                    fasta_text = st.text_area("Insert DNA sequence as a FASTA format",value = data)
+                else:
+                    fasta_text = st.text_area("Insert DNA sequence as a FASTA format",help='Must be in FASTA format!')
+    
             else:
-                fasta_text = st.text_area("Insert DNA sequence as a FASTA format",help='Must be in FASTA format!')
-
-        else:
-            fasta_file = st.file_uploader("Upload FASTA File",type=['txt','fasta'],help='Click on upload and select fasta/text file of your choice')
-            if name not in ss.data['Summary']['Project Name'].unique():
-                return
-            sequence = ss.data['Summary'][ss.data['Summary']['Project Name']==name]['Sequence'].to_list()[0]
-            fasta_data = f'>{name}\n{sequence}'
-            fasta_text = st.text_area("Insert DNA sequence as a FASTA format", value=fasta_data, help='Must be in FASTA format!')
-
-        submit = st.form_submit_button("Submit", use_container_width=True,help='Please check before submitting')
-        form_glass_bg()
-
-        if submit:
-            print('Submitted')
-            if project_name == '':
-                st.error('Project Name cannot be blank!')
-                return
-
-            data = fasta_text if fasta_text else fasta_file.read().decode("utf-8") if fasta_file!=None else ''
-
-            if data == '':
-                st.error('No data was recorded! Please Try Again')
-                return
-            # This part comes into picture only when the project is being modified
-            if (fasta_data == data and project_name == name and name != ''):
-                st.info('No changes were noticed!')
-                return
-
-            # This part comes into picture only when the project is being modified
-            elif (fasta_data.strip() == data.strip() and project_name != name and name != ''):
-                if check_name(ss.data['Summary'],project_name):
-                    st.write('Only project name was changed')
-                    ss.data = change_project_name(ss.data,name,project_name)
-                    st.success('Project Name was changed to '+project_name)
+                fasta_file = st.file_uploader("Upload FASTA File",type=['txt','fasta'],help='Click on upload and select fasta/text file of your choice')
+                if name not in ss.data['Summary']['Project Name'].unique():
                     return
-
-            # This part comes into picture when a new project is being added or modfied as well!
-            elif check_name(ss.data['Summary'],project_name,ss.page_state,name):
-                if validate_fasta(data.strip()):
-                    data = str(list(SeqIO.parse(StringIO(data), "fasta"))[0].seq)
-#                    print('Data:',data)
-                    st.success(f"Submitted")
-                    df = predict_efficacy_scores(data)
-                    print(df)
-                    results = 1
-            else:
-                return
+                sequence = ss.data['Summary'][ss.data['Summary']['Project Name']==name]['Sequence'].to_list()[0]
+                fasta_data = f'>{name}\n{sequence}'
+                fasta_text = st.text_area("Insert DNA sequence as a FASTA format", value=fasta_data, help='Must be in FASTA format!')
+    
+            submit = st.form_submit_button("Submit", use_container_width=True,help='Please check before submitting')
+            form_glass_bg()
+    
+            if submit:
+                print('Submitted')
+                if project_name == '':
+                    st.error('Project Name cannot be blank!')
+                    return
+    
+                data = fasta_text if fasta_text else fasta_file.read().decode("utf-8") if fasta_file!=None else ''
+    
+                if data == '':
+                    st.error('No data was recorded! Please Try Again')
+                    return
+                # This part comes into picture only when the project is being modified
+                if (fasta_data == data and project_name == name and name != ''):
+                    st.info('No changes were noticed!')
+                    return
+    
+                # This part comes into picture only when the project is being modified
+                elif (fasta_data.strip() == data.strip() and project_name != name and name != ''):
+                    if check_name(ss.data['Summary'],project_name):
+                        st.write('Only project name was changed')
+                        ss.data = change_project_name(ss.data,name,project_name)
+                        st.success('Project Name was changed to '+project_name)
+                        return
+    
+                # This part comes into picture when a new project is being added or modfied as well!
+                elif check_name(ss.data['Summary'],project_name,ss.page_state,name):
+                    if validate_fasta(data.strip()):
+                        data = str(list(SeqIO.parse(StringIO(data), "fasta"))[0].seq)
+    #                    print('Data:',data)
+                        st.success(f"Submitted")
+                        df = predict_efficacy_scores(data)
+                        print(df)
+                        results = 1
+                else:
+                    return
+    except:
+        return
 
     if results == 1:
         st.success("Processing complete! Redirecting to results...")
